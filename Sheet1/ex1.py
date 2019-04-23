a078f53560660f98c7d454d57ba7120302b63267
Oliver Mautschke <olivermautschke@gmail.com>
2019-04-23 20:18:43 +0200

    Sheet 1 started

--- Sheet1/ex1.py
+++ Sheet1/ex1.py
@@ -1,6 +1,44 @@
-import matplotlib.image as mpimg 
-import matplotlib.pyplot as plt 
+from PIL import Image
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy
 
-img = mpimg.imread('red-fox.png') 
-  
-plt.imshow(img) 
+pic = Image.open("red-fox.jpg")
+
+pix = numpy.array(pic)
+
+print("depth:", pic.bits, "width:", pic.size[0], "heigth:", pic.size[1])
+
+plt.imshow(pix)
+
+img = pic.resize((227, 227))
+
+pix2 = numpy.array(img)
+
+plt.imshow(pix2)
+
+lilpic = pic.crop((100, 327, 100, 327))
+pix3 = numpy.array(lilpic)
+
+#plt.imshow(pix3)
+
+
+#test = pic.convert('LA')
+
+#pix4 = numpy.array(list(test.getdata(band=0)), float)
+
+#plt.imshow(pix4)
+
+rot = pic.rotate(30, expand=1)
+
+pixi = numpy.array(rot)
+
+#plt.imshow(pixi)
+
+
+print(pic)
+
+
+
+plt.show()
+

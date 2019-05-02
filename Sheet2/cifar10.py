5c6026f4b9b66c80de832671b153bd8192ee794a
OMautschke <olivermautschke@gmail.com>
2019-05-02 19:48:52 +0200

    Sheet 2 added
    You have to add the cifar dataset in the root folder

---
+++ Sheet2/cifar10.py
@@ -0,0 +1,38 @@
+'''
+adapted from https://github.com/bagavi/CS231N/blob/master/assignment1/cs231n/data_utils.py
+'''
+
+import pickle
+import numpy as np
+import os
+
+def load_CIFAR_batch(filename):
+  """ load single batch of cifar """
+  with open(filename, 'rb') as f:
+    datadict = pickle.load(f,encoding='latin1')
+    X = datadict['data']
+    Y = datadict['labels']
+    X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
+    Y = np.array(Y)
+    return X, Y
+
+
+def load_CIFAR10(ROOT):
+  """ 
+  load all of cifar
+  
+  ROOT: path to the dataset files
+  """
+  
+  xs = []
+  ys = []
+  for b in range(1,6):
+    f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
+    X, Y = load_CIFAR_batch(f)
+    xs.append(X)
+    ys.append(Y)    
+  Xtr = np.concatenate(xs)#training samples
+  Ytr = np.concatenate(ys)#label of training samples
+  del X, Y
+  Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))#test samples
+  return Xtr, Ytr, Xte, Yte

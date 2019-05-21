b0530205df8f20a21d5ccfc962611ca53219356f
OMautschke <olivermautschke@gmail.com>
2019-05-22 00:29:12 +0200

    Hier fehlt noch der Vergleich zu pytorch

--- Sheet5/ex3.py
+++ Sheet5/ex3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def conv2d(picture, filter):
     convPicture = np.zeros(picture.shape)
@@ -11,6 +12,27 @@ def conv2d(picture, filter):
     return convPicture
 
 if __name__ == '__main__':
-    picture = np.random.rand(5, 5)
-    filter = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
-    print(conv2d(picture, filter))
+    filterA = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16
+    filterB = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    filterC = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+
+    pic = plt.imread('cameraman.png')
+    org = np.array(pic)
+
+    fA = conv2d(org, filterA)
+    fB = conv2d(org, filterB)
+    fC = conv2d(org, filterC)
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(org, 'gray')
+    plt.title('Original')
+    plt.subplot(2, 2, 2)
+    plt.imshow(fA, 'gray')
+    plt.title('Filter A')
+    plt.subplot(2, 2, 3)
+    plt.imshow(fB, 'gray')
+    plt.title('Filter B')
+    plt.subplot(2, 2, 4)
+    plt.imshow(fC, 'gray')
+    plt.title('Filter C')
+    plt.show()

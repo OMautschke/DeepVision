9e63dcbcf39a52c12be0f34d6efba82c37b5ed1f
Sebastian <sebastian.hegmann@hs-augsburg.de>
2019-06-23 01:34:38 +0200

    CNN, Training, Evaluation

--- Sheet8/CNN.py
+++ Sheet8/CNN.py
@@ -1,24 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
 
 
 class CNN(nn.Module):
 
-    def __init__(self, in_channel, out_channel):
+    def __init__(self):
         super(CNN, self).__init__()
 
         self.layer1 = nn.Sequential(
-            nn.Conv2d(6, 25, kernel_size=5, stride=1, padding=0),
+            nn.Conv2d(3, 6, kernel_size=5, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 25, kernel_size=5, stride=1, padding=0),
+            nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
-        self.fc1 = nn.Linear("?", 120)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -26,55 +29,74 @@ class CNN(nn.Module):
         out = self.layer1(x)
         out = self.layer2(out)
 
+        out = out.view(-1, 16 * 5 * 5)
+
         out = F.relu(self.fc1(out))
         out = F.relu(self.fc2(out))
-        out = F.relu(self.fc3(out))
+        out = self.fc3(out)
 
         return out
 
+    def train(self):
+        trainloader, testloader = self.load_data()
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
 
-"""
-https://www.learnopencv.com/number-of-parameters-and-tensor-sizes-in-convolutional-neural-network/
-Output Size:
-(64 x 64 x 1): Activation size = 64 * 64 * 1 = 4096
+        for epoch in range(10):
 
-Layer_1: 64 - 5 + 1 = 60 x 60 x 6
-MaxPool: (60 - 2) / 2 + 1 = 30 x 30 x 6
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data
 
-Layer_2: 30 - 5 + 1 = 26 x 26 x 16
-MaxPool: (30 - 2) / 2 + 1 = 15 x 15 x 16
+                optimizer.zero_grad()
 
-fc1: 120 x 1
-fc2:  84 x 1
-fc3:  20 x 1
+                outputs = cnn(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-Number of Parameters:
-Layer1:
-Weights = 5^2 x 1 x 6 = 150
-Biases = 6
-Parameters = 150 + 6 = 156
+    def load_data(self):
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+             ])
 
-Layer2:
-Weights = 5^2 x 1 x 16 = 400
-Biases = 16
-Parameters = 400 + 16 = 416
+        trainset = torchvision.datasets.CIFAR10(root='./Data',
+                                                train=True,
+                                                download=True,
+                                                transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset,
+                                                  batch_size=16,
+                                                  shuffle=True)
 
+        testset = torchvision.datasets.CIFAR10(root='./Data',
+                                               train=False,
+                                               download=True,
+                                               transform=transform)
+        testloader = torch.utils.data.DataLoader(testset,
+                                                 batch_size=16,
+                                                 shuffle=False)
 
-fc1 connected to a Conv Layer:
-Weights: 15^2 x 16 x 120 = 432000
-Biases:  120
-Parameters = 432120
+        return trainloader, testloader
 
+if __name__ == "__main__":
+    cnn = CNN()
+    cnn.train()
 
-fc2 connected to the previous fc layer:
-Weights: 120 x 84 = 10080
-Biases:  84
-Parameters: 10164
+    trainloader, testloader = cnn.load_data()
+    dataiter = iter(testloader)
 
-fc3 connected to the previous fc layer:
-Weights: 84 x 10 = 840
-Biases:  10
-Parameters: 850
+    total_correct = 0
+    total_images = 0
+    confusion_matrix = np.zeros([10, 10], int)
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = cnn(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total_images += labels.size(0)
+            total_correct += (predicted == labels).sum().item()
+            for i, l in enumerate(labels):
+                confusion_matrix[l.item(), predicted[i].item()] += 1
 
-Total Parameters: 156 + 416 + 432120 + 10164 + 850 = 443706
-"""
+    model_accuracy = total_correct / total_images * 100
+    print('Model accuracy on {0} test images: {1:.2f}%'.format(total_images, model_accuracy))

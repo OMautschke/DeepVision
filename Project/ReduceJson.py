9ba06a6bf22068099736edfcf52ac95121b525ae
OMautschke <olivermautschke@gmail.com>
2019-06-26 17:48:32 +0200

    Das Reduce Script für Json Files

---
+++ Project/ReduceJson.py
@@ -0,0 +1,23 @@
+import json
+
+reduced_file = {}
+
+with open('bdd100k_labels_images_val.json') as json_file:  
+    data = json.load(json_file)
+    for pic in data:
+        reduced_file[pic['name']] = {
+            'attriutes': pic['attributes'],
+            'objects': []     
+        }
+        for obj in pic['labels']:
+            if obj['category'] == 'lane' or obj['category'] == 'driveable_area':
+                continue
+            reduced_file[pic['name']]['objects'].append({
+                'category': obj['category'],
+                'box2d': obj['category']
+            })
+
+
+with open('reduced_labels.json', 'w') as outfile:  
+    json.dump(reduced_file, outfile)
+        

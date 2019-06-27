dcdd27d217a55b30be150430dfa703b53defef95
Sebastian <sebastian.hegmann@gmx.de>
2019-06-27 02:36:49 +0200

    loadData

--- Project/LoadData.py
+++ Project/LoadData.py
@@ -1,13 +1,47 @@
-import json
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
 
 class LoadData(object):
 
     def __init__(self):
-        pass
+        self.transform = transforms.Compose(
+            [
+             transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ]
+        )
 
-    def parse_lables(self, path):
-        with open(path) as json_file:
-            data = json.load(json_file)
-            for d in data:
-                pass
+    def load_data(self, path):
+        dataset = torchvision.datasets.ImageFolder(
+            root=path,
+            transform=self.transform
+        )
+
+        dirs = os.listdir(path)
+        _, _, files = next(os.walk(path + dirs[0]))
+        test_loader = torch.utils.data.DataLoader(
+            dataset.imgs[0:len(files)],
+            batch_size=1,   # Default 1
+            shuffle=False
+        )
+
+        offset = len(files)
+        _, _, files = next(os.walk(path + dirs[1]))
+        train_loader = torch.utils.data.DataLoader(
+            dataset.imgs[offset:(len(files) + offset)],
+            batch_size=1,   # Default 1
+            shuffle=True
+        )
+
+        offset = len(files)
+        _, _, files = next(os.walk(path + dirs[2]))
+        val_loader = torch.utils.data.DataLoader(
+            dataset.imgs[offset:(len(files) + offset)],
+            batch_size=1,   # Default 1
+            shuffle=False
+        )
+
+        return test_loader, train_loader, val_loader
--- Project/Main.py
+++ Project/Main.py
@@ -1,11 +1,16 @@
 from Project.LoadData import LoadData
 
 
-testPath = "Project/bdd100k/images/10k/"
+train_path = 'bdd100k/images/10k/'
 
 
 def main():
     load_data = LoadData()
+    test_loader, train_loader, val_loader = load_data.load_data(train_path)
+
+    for batch_idx, (data, target) in enumerate(val_loader):
+        print(target[0:16])
+        pass
 
 
 if __name__ == "__main__":

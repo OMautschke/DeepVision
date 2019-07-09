536fd86d0027cdfd86f431e9c0510b8095be3db5
Sebastian <sebastian.hegmann@gmx.de>
2019-07-10 00:56:00 +0200

    rpn

--- Project/Main.py
+++ Project/Main.py
@@ -3,13 +3,21 @@ from Project.FeatureExtractor import FeatureExtractor
 import torch
 from Project.RPN import RPN
 import matplotlib.pyplot as plt
-
+from torch.autograd import Variable
 
 test_path = 'bdd100k/images/10k/test/'
 train_path = 'bdd100k/images/10k/train/'
 val_path = 'bdd100k/images/10k/val/'
 
 create_feature_map = True
+
+activation = {}
+
+
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
 
 
 def feature_map():
@@ -23,6 +31,25 @@ def feature_map():
     model = FeatureExtractor(train_loader)
     model.load_state_dict(torch.load("./FeatureMap.pt"))
     model.eval()
+
+    rpn = RPN(512, 512, model.state_dict())
+
+    #model.conv_trans2.register_forward_hook(get_activation("conv_trans2"))
+    for n, p in model.named_parameters():
+        #print(n, p.shape)
+        pass
+
+
+def load_my_state_dict(self, state_dict):
+
+    own_state = self.state_dict()
+    for name, param in state_dict.items():
+        if name not in own_state:
+            continue
+        if isinstance(param, torch.nn.Parameter):
+            # backwards compatibility for serialized parameters
+            param = param.data
+        own_state[name].copy_(param)
 
 
 def main():
--- Project/RPN.py
+++ Project/RPN.py
@@ -7,15 +7,12 @@ import torchvision.transforms as transforms
 
 class RPN(nn.Module):
 
-    def __init__(self, in_channel, mid_channel, ratio=[0.5, 1, 2], anchor_size=[128, 256, 512]):
+    def __init__(self, in_channel, mid_channel, features, ratio=[0.5, 1, 2], anchor_size=[128, 256, 512]):
         super(RPN, self).__init__()
 
         self.ratio = ratio
         self.anchor_size = anchor_size
         self.k = len(ratio) * len(anchor_size)
-
-        self.rpn_conv = nn.Sequential(nn.Conv2d(in_channel, mid_channel, kernel_size=5, stride=1, padding=0),
-                                       nn.ReLU())
 
         # 9 anchor * 2 classfier (object or non-object) each grid
         self.class_score = nn.Conv2d(mid_channel, 2 * 9, kernel_size=1, stride=1)
@@ -24,8 +21,10 @@ class RPN(nn.Module):
         self.box_pred = nn.Conv2d(mid_channel, 4 * 9, kernel_size=1, stride=1)
         self.softmax = nn.Softmax()
 
+        self.features = features
+
     def forward(self, features):
-        features = self.rpn_conv(features)
+        #features = self.rpn_conv(features)
         #ligits, rpn_bbox_pred = self.conv1(features), self.conv2(features)
 
         _, _, height, width = features.shape
